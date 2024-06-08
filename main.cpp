0f02bbe8d64ada50c45f64b52de680f56e4c526b
Rana Muhammad Musab Akram <150699895+ranamusab1@users.noreply.github.com>
2024-06-08 13:44:27 +0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+using namespace std;
+
+char board[3][3] = { {'1', '2', '3'}, {'4', '5', '6'}, {'7', '8', '9'} };
+
+void displayBoard() {
+    cout << " " << board[0][0] << " | " << board[0][1] << " | " << board[0][2] << endl;
+    cout << "-----------" << endl;
+    cout << " " << board[1][0] << " | " << board[1][1] << " | " << board[1][2] << endl;
+    cout << "-----------" << endl;
+    cout << " " << board[2][0] << " | " << board[2][1] << " | " << board[2][2] << endl;
+}
+
+bool checkWin(char player) {
+    // Check rows
+    for (int i = 0; i < 3; i++) {
+        if (board[i][0] == player && board[i][1] == player && board[i][2] == player)
+            return true;
+    }
+    // Check columns
+    for (int i = 0; i < 3; i++) {
+        if (board[0][i] == player && board[1][i] == player && board[2][i] == player)
+            return true;
+    }
+    // Check diagonals
+    if ((board[0][0] == player && board[1][1] == player && board[2][2] == player) ||
+        (board[0][2] == player && board[1][1] == player && board[2][0] == player))
+        return true;
+    return false;
+}
+
+int main() {
+    char player = 'X';
+    int move;
+
+    while (true) {
+        displayBoard();
+        cout << "Player " << player << ", enter your move (1-9): ";
+        cin >> move;
+
+        // Update board
+        switch (move) {
+            case 1: board[0][0] = player; break;
+            case 2: board[0][1] = player; break;
+            case 3: board[0][2] = player; break;
+            case 4: board[1][0] = player; break;
+            case 5: board[1][1] = player; break;
+            case 6: board[1][2] = player; break;
+            case 7: board[2][0] = player; break;
+            case 8: board[2][1] = player; break;
+            case 9: board[2][2] = player; break;
+            default: cout << "Invalid move. Try again." << endl; continue;
+        }
+
+        // Check for win
+        if (checkWin(player)) {
+            displayBoard();
+            cout << "Player " << player << " wins!" << endl;
+            break;
+        }
+
+        // Switch player
+        player = (player == 'X') ? 'O' : 'X';
+    }
+
+    return 0;
+}
